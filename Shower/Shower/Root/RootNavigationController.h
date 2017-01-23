7109b68fdc3da218fe77fcf2241ad46315315b5a
FindThirteen <Thirteen@WangShuChaodeMacBook-Pro.local>
2017-01-23 15:50:10 +0800

    基础文件

---
+++ Shower/Shower/Root/RootNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  RootNavigationController.h
+//  Shower
+//
+//  Created by WangShuChao on 2017/1/23.
+//  Copyright © 2017年 无限未来. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootNavigationController : UINavigationController
+
+@end
---
+++ Shower/Shower/Root/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  Shower
+//
+//  Created by WangShuChao on 2017/1/23.
+//  Copyright © 2017年 无限未来. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
