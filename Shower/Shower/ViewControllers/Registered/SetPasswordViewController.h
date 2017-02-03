1ad75ed15199260880fa7fe921a87d96e03c4ead
Guochao <mark_4028@sina.com>
2017-02-03 14:31:53 +0800

    快速注册 -> 设置密码

---
+++ Shower/Shower/ViewControllers/Registered/SetPasswordViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetPasswordViewController.h
+//  Shower
+//
+//  Created by WangShuChao on 2017/2/3.
+//  Copyright © 2017年 无限未来. All rights reserved.
+// 快速注册 -> 设置密码
+
+#import "RootViewController.h"
+
+@interface SetPasswordViewController : RootViewController
+
+@end
---
+++ Shower/Shower/ViewControllers/Registered/SetPasswordViewController.m
@@ -0,0 +1,37 @@
+//
+//  SetPasswordViewController.m
+//  Shower
+//
+//  Created by WangShuChao on 2017/2/3.
+//  Copyright © 2017年 无限未来. All rights reserved.
+//快速注册 -> 设置密码
+
+#import "SetPasswordViewController.h"
+
+@interface SetPasswordViewController ()
+
+@end
+
+@implementation SetPasswordViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
